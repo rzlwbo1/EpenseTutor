6424ebc0bbe57cecf65f7bdca3a9c2a5590d3dfe
rzlwbo1 <rizalwb2@gmail.com>
2022-11-13 23:50:15 +0700

    header balance dkk

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className='container'>
+        <Balance />
+
+        <IncomeExpense />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
